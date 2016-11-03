672164fec1a3c5a9a4bd3b1ec1aaeb77e9f365d8
Atte <attekett@gmail.com>
2016-11-03 16:49:37 +0200

    Add docker swarm dependencies
    Fuzzvm should now contain required docker images and settings to build up the swarm.

--- fuzzvm/install.sh
+++ fuzzvm/install.sh
@@ -7,8 +7,22 @@
 sudo apt-get update;
 sudo apt-get install docker.io -y;
 
+sudo groupadd docker
+sudo gpasswd -a ${USER} docker
+sudo service docker restart
+
+
 # docker-machine
 sudo sh -c "curl -L https://github.com/docker/machine/releases/download/v0.8.2/docker-machine-`uname -s`-`uname -m` >/usr/local/bin/docker-machine && chmod +x /usr/local/bin/docker-machine"
+
+sudo docker pull swarm;
+sudo docker pull ubuntu;
+sudo docker pull progrium/consul;
+
+
+#Quick hack to get past docker-machine apt-get update
+sudo ln -s /bin/true /usr/local/bin/apt-get
+
 
 mkdir -p $HOME/.ssh;
 mv /tmp/fuzzvm-key $HOME/.ssh/id_rsa
