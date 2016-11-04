b1ae4d45eb8fb23e8f18c4c49715d10c522a1463
Atte <attekett@gmail.com>
2016-11-04 12:42:39 +0200

    Change install.sh to use docker from apt-repository

--- fuzzvm/install.sh
+++ fuzzvm/install.sh
@@ -3,9 +3,23 @@
 ##
 ##FuzzVM install script.
 ##
+##Note: This script is for Ubuntu 16.04
+
+REPO="deb https://apt.dockerproject.org/repo ubuntu-xenial main"
+
+sudo apt-key adv --keyserver hkp://p80.pool.sks-keyservers.net:80 --recv-keys 58118E89F3A912897C070ADBF76221572C52609D
+
+echo "$REPO" | sudo tee /etc/apt/sources.list.d/docker.list
 
 sudo apt-get update;
-sudo apt-get install docker.io -y;
+
+sudo apt-get install -y apt-transport-https ca-certificates;
+
+
+
+sudo apt-get install linux-image-extra-$(uname -r) linux-image-extra-virtual -y;
+
+sudo apt-get install docker-engine -y;
 
 sudo groupadd docker
 sudo gpasswd -a ${USER} docker
