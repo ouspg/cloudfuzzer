e2b8cb5209bff5dae2704ac03637eb1534319b46
Atte <attekett@gmail.com>
2016-11-08 12:37:56 +0200

    Add bastion swarm-create logic

--- bastion/setup-swarm.sh
+++ bastion/setup-swarm.sh
@@ -4,6 +4,29 @@
 ##Swarm setup script
 ##
 
-#IP=$(hostname -i)
+ADDRESSES="$@"
 
-#docker-machine create --driver generic --generic-ip-address $IP local
+echo "FuzzVMs: ADDRESSES"
+
+echo "Checking connection to all FuzzVMs."
+
+for fuzzvm in $ADDRESSES; do
+	echo "Checking $fuzzvm"
+	ssh $fuzzvm -o ConnectTimeout=10 'echo -n $HOSTNAME\($(hostname -I | awk '\''{print $1}'\'')\);' && echo " - Online"
+	if [ $? -ne 0 ]; then
+		echo "Couldn't reach host: $fuzzvm"
+		echo "Before rerunning this script:"
+		echo "Check that you provided valid address for FuzzVM instance, in a form that can be used in ssh <address>."
+		echo "Also check that you are using correct username, that has the ssh-keys set."
+		exit;
+	fi
+done
+
+SWARM_MASTER=$1;
+shift;
+SWARM_NODES=$@;
+
+echo "Selected $SWARM_MASTER as swarm master"
+echo "Starting swarm-create.sh on swarm master"
+ssh $SWARM_MASTER "scripts/swarm-create.sh $SWARM_NODES";
+
