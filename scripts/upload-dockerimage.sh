116d382bddf919d23afefe8cc8c5ddefac1dc8bf
Mikko Yliniemi <mikkoyli@paju.oulu.fi>
2016-11-10 17:13:28 +0200

    Added script for uploading dockerimage

---
+++ scripts/upload-dockerimage.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+#usage: upload-dockerimage.sh <bastion> <fuzzvm>
+
+ADDRESSES="$@"
+
+echo "Bastion: $1"
+
+## Todo: this must be modified, it's currently very slow and does same job multiple times
+
+for i in ${@:2}
+do
+    echo "FuzzVM: $i"
+    docker save testi/myapp  | gzip  | pv | ssh ubuntu@$1 "cat | pv | ssh $i \"docker load\" "
+done
