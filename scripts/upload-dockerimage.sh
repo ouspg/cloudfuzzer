ad0272b8ff7ac63869ef2c91ade6bc12445ab655
Mikko Yliniemi <mikkoyli@paju.oulu.fi>
2016-11-16 14:49:16 +0200

    updated uplaod script

--- scripts/upload-dockerimage.sh
+++ scripts/upload-dockerimage.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-#usage: upload-dockerimage.sh <docker-image> <bastion> <fuzzvm1> <fuzzvm2> ...
+#usage: upload-dockerimage.sh <docker-image> <bastion>
 
-#example: ./upload-dockerimage.sh <docker-image> <bastion> `./get-gcloud-fuzzvm-ips.sh`
+#example: ./upload-dockerimage.sh <docker-image> <bastion>
 
 echo "Image name: $1"
 echo "Bastion: $2"
@@ -10,5 +10,5 @@ echo "Bastion: $2"
 for i in ${@:3}
 do
     echo "FuzzVM: $i"
-    docker save $1  | gzip  | pv | ssh ubuntu@$2 "cat | pv | ssh $i \"docker load\" " &
+    docker save $1  | gzip  | pv | ssh ubuntu@$2 "cat | pv | ssh $i \"docker load\" "
 done
