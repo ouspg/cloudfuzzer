8b131b36a06f82cea739ecf70d870f49ea6e9d5a
Mathias Nyman <mathias.nyman@iki.fi>
2016-11-23 10:45:36 +0200

    Add bastion ssh key to authorized_keys on bastion

--- bastion/install.sh
+++ bastion/install.sh
@@ -13,6 +13,7 @@ set -o nounset
 mkdir -p $HOME/.ssh;
 mv /tmp/bastion-key $HOME/.ssh/id_rsa
 chmod 600 $HOME/.ssh/id_rsa
+cat /tmp/bastion-key.pub >> $HOME/.ssh/authorized_keys
 cat /tmp/fuzzvm-key.pub >> $HOME/.ssh/authorized_keys
 
 mv /tmp/scripts $HOME/
