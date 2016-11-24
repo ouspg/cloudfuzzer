19f72e36d02e4152a28b6d1496eef1acb815a62d
Mathias Nyman <mathias.nyman@iki.fi>
2016-11-24 09:15:24 +0200

    Remove ssh password authentication

--- bastion/install.sh
+++ bastion/install.sh
@@ -16,4 +16,5 @@ cat /tmp/fuzzvm-key.pub >> $HOME/.ssh/authorized_keys
 mv /tmp/scripts $HOME/
 
 sudo passwd ubuntu -l
+sudo sed -i 's/#PasswordAuthentication yes/PasswordAuthentication no/' /etc/ssh/sshd_config
 
