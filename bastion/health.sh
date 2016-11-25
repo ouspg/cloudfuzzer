88d522c1bc7668d306781c83971bdf42f81dfb0a
Mikko Yliniemi <mikkoyli@paju.oulu.fi>
2016-11-25 13:36:24 +0200

    updated health script

--- bastion/health.sh
+++ bastion/health.sh
@@ -1,3 +1,13 @@
 #!/bin/bash
 
-#todo: check all nodes connected to discovery
+#Check all nodes connected to discovery
+
+#Todo: Need improvement
+
+MASTER=`cat $HOME/address_master`
+
+for node in $(cat $HOME/address_nodes)
+do
+    echo "Checking node: $node"
+    ssh $MASTER "docker -H :4000 info |grep -A 2 $node|grep Status"
+done
