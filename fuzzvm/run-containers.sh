64f9894fb8a343f3d1e2fb5de93127a210a41564
Mikko Yliniemi <mikkoyli@paju.oulu.fi>
2017-02-03 11:33:46 +0200

    added volumes-from

--- fuzzvm/run-containers.sh
+++ fuzzvm/run-containers.sh
@@ -4,5 +4,5 @@ COUNT=$1
 
 for instance in $(seq 1 $COUNT); do
 	echo "Starting instance: $instance"
-	docker -H :4000 run $(cat $HOME/docker-options);
+	docker -H :4000 run -d --volumes-from rsync-volume-container $(cat $HOME/docker-options);
 done
