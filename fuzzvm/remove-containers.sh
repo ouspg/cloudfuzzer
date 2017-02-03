1ff04d574ad25091369842162fb747d1242d9f51
Kettunen, Atte <atte.kettunen@f-secure.com>
2017-02-03 10:12:54 +0200

    Add fuzzvm side remove and stop containers scripts

---
+++ fuzzvm/remove-containers.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+set -o errexit
+set -o nounset
+
+#Remove stopped fuzzing containers from our cluster.
+
+NODE_ADDRESSES=$(cat $HOME/address_nodes);
+BASTION_ADDRESS=$(cat $HOME/address_bastion);
+echo "Stopping containers:" 1>&2;
+for node in $NODE_ADDRESSES; do
+	echo $node 1>&2;
+	for container in $(docker -H $node:2375 ps -a | sed -e "1d" | awk  '$2 != "swarm" && $2 != "nabeken/docker-volume-container-rsync" && $2 != "progrium/consul" {print $1}'); do
+		echo $container 1>&2;
+		docker -H $node:2375 rm $container;
+	done
+done
