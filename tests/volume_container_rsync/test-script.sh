0890b77885259007259ff07e7c3e8e1cb7f444b4
Kettunen, Atte <atte.kettunen@f-secure.com>
2017-02-03 10:27:30 +0200

    Add output directory create to volume_container_rsync test script

--- tests/volume_container_rsync/test-script.sh
+++ tests/volume_container_rsync/test-script.sh
@@ -4,6 +4,9 @@
 echo "Starting test...";
 
 #We just write dummy-files to the samples and results folders.
+mkdir -p /output/samples;
+mkdir -p /output/results;
+
 count=0;
 SCID=$(cat /etc/hostname);
 while true; do
