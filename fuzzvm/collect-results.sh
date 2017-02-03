6aec49d7e0acfbc7c22f3c7325ec544507c2b5b5
Kettunen, Atte <atte.kettunen@f-secure.com>
2017-02-03 10:05:00 +0200

    Update black-list for non-rsync sample/result collection

--- fuzzvm/collect-results.sh
+++ fuzzvm/collect-results.sh
@@ -12,7 +12,7 @@ BASTION_ADDRESS=$(cat $HOME/address_bastion);
 echo "Getting results." 1>&2;
 for node in $NODE_ADDRESSES; do
 	echo $node 1>&2;
-	for container in $(docker -H $node:2375 ps -a | sed -e "1d" | awk  '$2 != "swarm" && $2 != "progrium/consul" {print $1}'); do
+	for container in $(docker -H $node:2375 ps -a | sed -e "1d" | awk  '$2 != "swarm" && $2 != "nabeken/docker-volume-container-rsync" && $2 != "progrium/consul" {print $1}'); do
 		echo $container 1>&2;
 		docker -H $node:2375 cp $container:/results - | gzip |\
 		ssh -o StrictHostKeyChecking=no $BASTION_ADDRESS 'mkdir -p $HOME/results/'$container' && tar --strip-components=1 --skip-old-files -xzf - -C $HOME/results/'$container;
--- fuzzvm/collect-samples.sh
+++ fuzzvm/collect-samples.sh
@@ -12,7 +12,7 @@ BASTION_ADDRESS=$(cat $HOME/address_bastion);
 echo "Getting samples." 1>&2
 for node in $NODE_ADDRESSES; do
 	echo $node 1>&2;
-	for container in $(docker -H $node:2375 ps -a | sed -e "1d" | awk  '$2 != "swarm" && $2 != "progrium/consul" {print $1}'); do
+	for container in $(docker -H $node:2375 ps -a | sed -e "1d" | awk  '$2 != "swarm" && $2 != "nabeken/docker-volume-container-rsync" && $2 != "progrium/consul" {print $1}'); do
 		echo $container 1>&2;
 		ssh -o StrictHostKeyChecking=no $node "scripts/get-samples-from-container.sh $container" | \
 		ssh -o StrictHostKeyChecking=no $BASTION_ADDRESS 'mkdir -p $HOME/samples/'$container' && tar --strip-components=1 --skip-old-files -xzf - -C $HOME/samples/'$container;
