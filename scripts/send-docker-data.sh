5849f8d74c20bbd657c0e9fa51f076158b03315d
Kettunen, Atte <atte.kettunen@f-secure.com>
2017-03-03 16:32:13 +0200

    Update send-docker-data.sh

--- scripts/send-docker-data.sh
+++ scripts/send-docker-data.sh
@@ -3,23 +3,40 @@
 set -o errexit
 set -o nounset
 
-#Usage: bash send-docker-data.sh <bastion-address> <directory-with-docker-data>
-#Example: bash send-docker-data.sh ubuntu@bastion ./fuzz-data
+#Usage: bash send-docker-data.sh <directory-with-docker-data>
+#Example: bash send-docker-data.sh ./fuzz-data
 
 #Requirements:
-# fuzz-data directory must have the following structure:
-#	fuzz-data:
+# docker-data-directory must have the following structure:
+#	docker-data-directory:
 #		docker-image
-#		docker-options
+#		docker-arguments
 
 #docker-image:
-#	docker image saved with: docker save <image | gzip > docker-image
+#	docker image saved with: docker save <image> | gzip > docker-image
 
-#docker-options:
-#	one liner docker run options.
-#	Is used in swarm manager: docker run -d --volumes-from rsync-volume-container $(cat docker-options)
-BASTION=$1;
-DIRECTORY=$2;
+#docker-arguments:
+#	docker run options: docker run <arguments>
+#	
 
-scp -o StrictHostKeyChecking=no -o User=ubuntu -Cr $2/* $BASTION:
-ssh -o StrictHostKeyChecking=no -o User=ubuntu $BASTION 'scripts/distribute-local-docker-image.sh ./docker-image'
+if [ -z "$1" ]; then
+	echo "Missing required argument."
+	echo "You must specify the docker-data-directory."
+	return 1
+fi 
+
+DIRECTORY="$1";
+
+if [ ! -f "$DIRECTORY/docker-arguments" ]; then
+	echo "docker-arguments: No such file."
+	echo "docker-arguments file should contain arguments that are given to docker run"
+	echo "docker run <argument-to-run-fuzzer>"
+	return 1
+elif [ ! -f "$DIRECTORY/docker-image" ]; then
+	echo "docker-image: No such file."
+	echo "To save the image, you can use: docker save <image> | gzip > docker-image"
+	return 1
+fi
+
+scp -o StrictHostKeyChecking=no -o User=$BASTION_USER -Cr $2/* $BASTION_ADDRESS:
+ssh -o StrictHostKeyChecking=no -o User=$BASTION_USER $BASTION_ADDRESS 'scripts/helpers.sh distribute-local-docker-image ./docker-image'
