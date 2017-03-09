0e49475e9209d97153dbf3601e8fa0aedf9d6588
Kettunen, Atte <atte.kettunen@f-secure.com>
2017-03-09 15:39:10 +0200

    Add --min-size to get-results to prevent sync of zero size files

--- bastion/get-results.sh
+++ bastion/get-results.sh
@@ -12,5 +12,5 @@ fi
 
 # Rsync nodes
 for node in $NODES; do
-	rsync -auP --no-links rsync://$node:10873/volume/ results/
+	rsync -auP --no-links --min-size=1 rsync://$node:10873/volume/ results/
 done
